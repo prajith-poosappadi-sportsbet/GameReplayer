77b8bcdc55a3ab8c0a21a5b6198e776cd5814bd0
Prajith <prajith.poosappadi@sportsbet.com.au>
2023-01-09 14:32:06 +1100

    Remove unnecessary fields

--- GameReplayer/Helpers/GameHelper.cs
+++ GameReplayer/Helpers/GameHelper.cs
@@ -5,7 +5,6 @@ namespace GameReplayer.Helpers
     public static class GameHelper
     {
         public static readonly Uri BaseAddress = new Uri("https://nfl-ui-dev.int.ts.dev.sbet.cloud/");
-        // public static Uri  localbaseAddress = new Uri("http://localhost:31700/");
         public const string Cookie = "";
     }
 }
